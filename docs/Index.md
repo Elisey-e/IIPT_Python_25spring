a1748ee9480a084097ae48093f88e5ce5a5cdca6
Elisey <ez1071@yandex.ru>
2025-04-01 22:39:50 +0300

    First approach to documentation

---
+++ docs/Index.md
@@ -0,0 +1,9 @@
+# Добро пожаловать в MyProject
+
+MyProject - это мощный инструмент для...
+
+## Быстрый старт
+
+```python
+from myproject import main
+main.run()
---
+++ docs/conf.py
@@ -0,0 +1,25 @@
+master_doc = 'Index'
+
+extensions = [
+    'myst_parser',
+    'sphinx_rtd_theme',
+    'autodoc2',
+    'sphinx_click',
+    'matplotlib.sphinxext.plot_directive'
+]
+
+html_theme = 'sphinx_rtd_theme'
+
+autodoc2_packages = [
+    "../myproject",
+]
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+]
+
+source_suffix = {
+    '.md': 'markdown',
+}
