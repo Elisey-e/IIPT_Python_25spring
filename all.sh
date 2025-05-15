2288750af7739733dbc3fcf6bb8c90e429827a0a
Elisey <ez1071@yandex.ru>
2025-05-15 08:25:33 +0300

    added pdf doc

---
+++ all.sh
@@ -0,0 +1,7 @@
+poetry run python src/imgvisint/transform.py --help
+
+poetry run python src/imgvisint/transform.py --size 100 100 --input images
+
+poetry run python src/imgvisint/interpolation.py
+
+poetry run python src/imgvisint/sizebench.py
